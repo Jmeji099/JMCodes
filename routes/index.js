4c2110446dd238867fb798794d316aefc2682188
Juan Mejia <Jmejiaa1999@gmail.com>
2023-12-18 21:04:49 -0700

    Here we go I think this was the issue

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (req, res) =>   { 
-    res.status(200).json({
-        mgs: "Succesfully hit server"});
-});
-
-module.exports = router;
